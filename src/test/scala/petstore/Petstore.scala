1ff906a5187677a6dc4acce8cf4ea953b0b4c7e6
kazimir-malevich <adrian_lewis@hushmail.com>
2022-06-24 18:59:40 +0100

    first commit

---
+++ src/test/scala/petstore/Petstore.scala
@@ -0,0 +1,29 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import java.util.concurrent.ThreadLocalRandom
+
+class Petstore extends Simulation {
+
+  val httpProtocol =
+    http.baseUrl("https://petstore.octoperf.com")
+      .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+      .acceptLanguageHeader("en-US,en;q=0.5")
+      .acceptEncodingHeader("gzip, deflate")
+      .userAgentHeader(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
+      )
+
+    val scn = scenario("Petstore")
+      .exec(
+        http("Home").get("/")
+          .check((status is 200), substring("Welcome to JPetStore"))
+      )
+     .pause(1)
+
+  setUp(
+    scn.inject(atOnceUsers(2)).protocols(httpProtocol)
+  ).protocols(httpProtocol)
+}

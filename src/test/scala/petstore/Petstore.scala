d0ae32a51bfdf619577a7d099aa58ea99015f5b3
kazimir-malevich <adrian_lewis@hushmail.com>
2022-06-25 09:22:54 +0100

    register

--- src/test/scala/petstore/Petstore.scala
+++ src/test/scala/petstore/Petstore.scala
@@ -3,9 +3,12 @@ package petstore
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-import java.util.concurrent.ThreadLocalRandom
+import scala.concurrent.duration._
+import scala.util.Random
 
 class Petstore extends Simulation {
+
+  def random(len: Int): String = Random.alphanumeric.filter(_.isLetter).take(len).mkString
 
   val httpProtocol =
     http
@@ -34,6 +37,7 @@ class Petstore extends Simulation {
     )
     .pause(1)
 
+
     .exec(
       http("Click on ‘Sign In’")
         .get("/actions/Account.action?signonForm=")
@@ -47,6 +51,35 @@ class Petstore extends Simulation {
         .check((status is 200), substring("User Information"))
     )
     .pause(1)
+
+		val staticVal = random(5)
+
+		exec(
+			http("Register Now")
+				.post("/actions/Account.action")
+				.formParam("username", staticVal)
+				.formParam("password", "password")
+				.formParam("repeatedPassword", "password" )
+				.formParam("account.firstName", "aidy")
+				.formParam("account.lastName", "lewis")
+				.formParam("account.email", "x@x.com")
+				.formParam("account.phone", "07123456789")
+				.formParam("account.address", "139 Kings Road")
+				.formParam("account.address2", "")
+				.formParam("account.city", "Ashton")
+				.formParam("account.state", "MCR")
+				.formParam("account.zip", "OL68EZ")
+				.formParam("account.country", "England")
+				.formParam("account.languagePreference", "English")
+				.formParam("account.favouriteCategoryId", "FISH")
+				.formParam("account.newAccount", "Save Account Information")
+				.formParam("_sourcePage", "9J-iMAXq9dG0LMgE4V3KSSKTw9srBcUe0SbJOrsq3vIUNciL00U9DRFBZpACw8IWp49AzOLT6WBBlcG8fI2ilrdMRlPVzDikCyGTNlKQI7o=")
+				.formParam("__fp", "ozWOy1XVolzhao1yxtUN88Q2a14M_zok4D5hqk6q1kUfbDiktNlmCs6LZ-WlER3rZK-tPmeQ3horzSEeVE0vGQ_XaqoIGx5lOV8PdoIUljGC7qqgdLiBR58ZR9T6iF4Yjczs3K3rguWbuBRHP6p6XHoIvs74I8v7DerwfeNtWLRpO-WSLIUTJi4qD0luMle")
+        .check((status is 200), substring("Saltwater, Freshwater"))
+		)
+
+  
+    
 
   setUp(
     scn.inject(atOnceUsers(2)).protocols(httpProtocol)
